f8b864ba3a085cee141f7a2766a0177629207c83
Edyfiva <edyfiva@gmail.com>
2022-12-19 13:59:37 -0300

    start project

---
+++ captacao/src/main/java/com/br/dbc/captacao/CaptacaoApplication.java
@@ -0,0 +1,13 @@
+package com.br.dbc.captacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptacaoApplication.class, args);
+	}
+
+}
---
+++ captacao/src/test/java/com/br/dbc/captacao/CaptacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.dbc.captacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
