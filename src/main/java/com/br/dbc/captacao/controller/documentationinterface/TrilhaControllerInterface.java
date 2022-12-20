bd4647d409b2e78c56cccbd6d203f54d8802ef08
Julio Rocha <juliogabrielmr@gmail.com>
2022-12-20 15:02:55 -0300

    Trilha controller pronta e documentada

---
+++ src/main/java/com/br/dbc/captacao/controller/documentationinterface/TrilhaControllerInterface.java
@@ -0,0 +1,47 @@
+package com.br.dbc.captacao.controller.documentationinterface;
+
+import com.br.dbc.captacao.dto.trilha.TrilhaCreateDTO;
+import com.br.dbc.captacao.dto.trilha.TrilhaDTO;
+import com.br.dbc.captacao.exception.RegraDeNegocioException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public interface TrilhaControllerInterface {
+
+    @Operation(summary = "Cadastrar nova Trilha", description = "Cadastro de nova Trilha")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Trilha cadastrada com sucesso"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            })
+    @PostMapping
+    ResponseEntity<TrilhaDTO> create(@RequestBody TrilhaCreateDTO trilhaCreateDTO);
+
+    @Operation(summary = "Listar Trilhas", description = "Lista todas Trilhas do banco de dados")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Trilha cadastrada com sucesso"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @GetMapping("/listar")
+    ResponseEntity<List<TrilhaDTO>> listarTodos();
+
+    @Operation(summary = "Deletar Trilha", description = "Deletar Trilha por ID")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Trilha excluída com sucesso"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @DeleteMapping
+    void delete(@RequestParam Integer idTrilha) throws RegraDeNegocioException;
+}
