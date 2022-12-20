53f327b127b71ede3dd433ee3d856e22fd4f2755
Matheus Goncalves <math3usgoncalves@protonmail.com>
2022-12-20 14:36:42 -0300

    fix: persistence package

--- src/main/java/com/br/dbc/captacao/entity/EntrevistaEntity.java
+++ src/main/java/com/br/dbc/captacao/entity/EntrevistaEntity.java
@@ -9,7 +9,6 @@ import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
--- src/main/java/com/br/dbc/captacao/entity/LinguagemEntity.java
+++ src/main/java/com/br/dbc/captacao/entity/LinguagemEntity.java
@@ -7,7 +7,6 @@ import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
 import java.util.Set;
 
 @AllArgsConstructor
