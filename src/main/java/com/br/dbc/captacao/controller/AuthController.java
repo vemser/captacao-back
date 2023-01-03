ace82094ed61c086c3bf3626bc9ad7a38d9a1f3c
Matheus Goncalves <math3usgoncalves@protonmail.com>
2023-01-03 15:37:14 -0300

    add: Auth controller

--- src/main/java/com/br/dbc/captacao/controller/AuthController.java
+++ src/main/java/com/br/dbc/captacao/controller/AuthController.java
@@ -1,12 +1,12 @@
 package com.br.dbc.captacao.controller;
 
-import br.com.vemser.facetoface.controller.documentationinterface.AuthControllerInterface;
-import br.com.vemser.facetoface.dto.login.LoginDTO;
-import br.com.vemser.facetoface.entity.UsuarioEntity;
-import br.com.vemser.facetoface.exceptions.RegraDeNegocioException;
-import br.com.vemser.facetoface.security.TokenService;
-import br.com.vemser.facetoface.service.AuthService;
-import br.com.vemser.facetoface.service.UsuarioService;
+import com.br.dbc.captacao.controller.documentationinterface.AuthControllerInterface;
+import com.br.dbc.captacao.dto.login.LoginDTO;
+import com.br.dbc.captacao.entity.GestorEntity;
+import com.br.dbc.captacao.exception.RegraDeNegocioException;
+import com.br.dbc.captacao.security.TokenService;
+import com.br.dbc.captacao.service.AuthService;
+import com.br.dbc.captacao.service.GestorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,28 +20,28 @@ import javax.validation.Valid;
 @Validated
 @RequiredArgsConstructor
 public class AuthController implements AuthControllerInterface {
-    private final UsuarioService usuarioService;
+    private final GestorService gestorService;
     private final TokenService tokenService;
     private final AuthService authService;
 
     @PostMapping("/fazer-login")
     public ResponseEntity<String> auth(@RequestBody @Valid LoginDTO loginDTO) {
-        UsuarioEntity usuarioEntity = authService.auth(loginDTO);
-        return new ResponseEntity<>(tokenService.getToken(usuarioEntity, null), HttpStatus.CREATED);
+        GestorEntity gestorEntity = authService.auth(loginDTO);
+        return new ResponseEntity<>(tokenService.getToken(gestorEntity, null), HttpStatus.CREATED);
     }
 
-    @PostMapping("/solicitar-troca-senha")
-    public void trocarSenha(@RequestParam @Valid String email) throws RegraDeNegocioException {
-        authService.trocarSenha(email);
-        new ResponseEntity<>(null, HttpStatus.CREATED);
-    }
-
-    @PostMapping("/trocar-senha")
-    public void trocarSenhaAuntenticado(@RequestParam String token) throws RegraDeNegocioException {
-        String email = authService.procurarUsuario(token);
-        usuarioService.atualizarSenhaUsuario(email);
-        new ResponseEntity<>(null, HttpStatus.CREATED);
-    }
+//    @PostMapping("/solicitar-troca-senha")
+//    public void trocarSenha(@RequestParam @Valid String email) throws RegraDeNegocioException {
+//        authService.trocarSenha(email);
+//        new ResponseEntity<>(null, HttpStatus.CREATED);
+//    }
+//
+//    @PostMapping("/trocar-senha")
+//    public void trocarSenhaAuntenticado(@RequestParam String token) throws RegraDeNegocioException {
+//        String email = authService.procurarUsuario(token);
+//        gestorService.atualizarSenhaUsuario(email);
+//        new ResponseEntity<>(null, HttpStatus.CREATED);
+//    }
 
     @PutMapping("/confirmar-entrevista")
     public void confirmarEntrevista(@RequestParam @Valid String tokenEntrevista) throws RegraDeNegocioException {
---
+++ src/main/java/com/br/dbc/captacao/controller/documentationinterface/AuthControllerInterface.java
@@ -0,0 +1,61 @@
+package com.br.dbc.captacao.controller.documentationinterface;
+
+import com.br.dbc.captacao.dto.login.LoginDTO;
+import com.br.dbc.captacao.exception.RegraDeNegocioException;
+import freemarker.template.TemplateException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
+import javax.validation.Valid;
+import java.io.IOException;
+
+public interface AuthControllerInterface {
+
+    @Operation(summary = "Logar com um usuário.", description = "Loga no sistema com um login de usuário.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Loga no sistema com um login de usuário."),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @PostMapping
+    ResponseEntity<String> auth(@RequestBody @Valid LoginDTO loginDTO);
+
+//    @Operation(summary = "Solicitar uma troca de senha do usuário.", description = "Solicita uma troca de senha do usuário.")
+//    @ApiResponses(
+//            value = {
+//                    @ApiResponse(responseCode = "201", description = "Solicita uma troca de senha do usuário."),
+//                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+//                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+//            }
+//    )
+//    @PostMapping
+//    void trocarSenha(@RequestParam @Valid String email) throws RegraDeNegocioException, MessagingException, TemplateException, IOException;
+
+//    @Operation(summary = "Inserir um token válido para troca de senha do usuário.", description = "Insira um token válido para troca de senha do usuário.")
+//    @ApiResponses(
+//            value = {
+//                    @ApiResponse(responseCode = "201", description = "Troca de senha do usuário."),
+//                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+//                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+//            }
+//    )
+//    @PostMapping
+//    void trocarSenhaAuntenticado(@RequestBody @Valid String token) throws RegraDeNegocioException, MessagingException, TemplateException, IOException;
+
+    @Operation(summary = "Confirmar uma entrevista agendada.", description = "Confirma uma entrevista agendada.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Entrevista confirmada."),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @PutMapping
+    void confirmarEntrevista(@PathVariable @Valid String tokenEntrevista) throws RegraDeNegocioException;
+}
