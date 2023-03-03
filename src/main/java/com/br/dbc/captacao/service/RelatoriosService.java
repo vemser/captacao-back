273d7a57ee485f0331bee7fafbbf60f015d0a0a4
Maicon Gerardi <maicon.gerardi@gmail.com>
2023-03-03 14:51:40 -0300

    feat: ajustar delete de inscriçao

--- src/main/java/com/br/dbc/captacao/service/RelatoriosService.java
+++ src/main/java/com/br/dbc/captacao/service/RelatoriosService.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class RelatoriosService {
+
     private final CandidatoRepository candidatoRepository;
     private final FormularioRepository formularioRepository;
 
